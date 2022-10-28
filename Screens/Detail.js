b32ecb96ca5f37ad020ed151006182a2071000df
hparvez905 <hparvez905@gmail.com>
2022-10-29 03:22:08 +0600

    detail & detail style added

--- Screens/Detail.js
+++ Screens/Detail.js
@@ -1,12 +1,75 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text,TextInput,StyleSheet,Pressable } from 'react-native'
+import React ,{useState} from 'react';
+import {firebase} from '../config';
+import { useNavigation } from '@react-navigation/native';
 
-const Detail = () => {
-  return (
-    <View>
-      <Text>Detail</Text>
-    </View>
-  )
+const Detail = (route) => {
+    const notesRef =firebase.firestore().collection('notes');
+    const  [textHeading, onChangeHeadingText] =useState(route.params.item.name);
+    const navigation =useNavigation();
+
+    const updateNote =()=>{
+        if (textHeading && textHeading.length >0){
+            notesRef
+            .doc(route.params.item.id)
+            .update({
+                heading: textHeading,
+            }).then (()=>{
+                navigation.navigate('Home')
+            }).catch((error)=>{
+                alert(error.message)
+            })
+        }
+    }
+    return (
+        <View style={styles.container}>
+            <TextInput
+            style={styles.textField}
+            onChangeText={onChangeHeadingText}
+            value={textHeading}
+            placeholder='update Note'
+            
+            />
+            <Pressable 
+            style={styles.buttonUpdate}
+            onPress={()=>{updateNote()}}
+            >
+                <Text> Update Note</Text>
+
+            </Pressable>
+
+        </View>
+    )
+    
 }
 
 export default Detail;
+
+
+const styles =StyleSheet.create({
+    container:{
+        marginTop:80,
+        marginLeft:15,
+        marginRight:15,
+    },
+    textField:{
+        marginBottom:10,
+        padding:10,
+        fontSize:15,
+        color:'#000000',
+        backgroundColor:'#e0e0e0',
+        borderRadius:5
+
+    },
+    buttonUpdate:{
+        marginTop:25,
+        alignItems:'center',
+        justifyContent:'center',
+        paddingVertical:12,
+        paddingHorizontal:32,
+        borderRadius:4,
+        elevation:10,
+        backgroundColor:'#0de065',
+
+    }
+})
