30e09b77dffb65b39e17d3dd9682ec1953201910
hparvez905 <hparvez905@gmail.com>
2022-10-29 03:00:54 +0600

    Home page & style done

--- Screens/Home.js
+++ Screens/Home.js
@@ -126,3 +126,60 @@ const Home = () => {
 }
 
 export default Home;
+
+
+const styles= StyleSheet.create({
+    container:{
+        backgroundColor:'#e5e5e5',
+        padding:15,
+        borderRadius:15,
+        margin:5,
+        marginHorizontal:10,
+        flexDirection:'row',
+        alignItems:'center'
+    },
+    innerContainer:{
+        alignItems:'center',
+        flexDirection:'column',
+        marginLeft:45,
+    },
+    itemHeading:{
+        fontWeight:'bold',
+        fontSize:18,
+        marginRight:22,
+    },
+    formContainer:{
+        flexDirection:'row',
+        height:80,
+        marginLeft:10,
+        marginRight:10,
+        marginTop:10,
+    },
+    input:{
+        height:48,
+        borderRadius:5,
+        overflow:'hidden',
+        backgroundColor:'white',
+        paddingLeft:16,
+        flex:1,
+        marginRight:5,
+    },
+    button:{
+        height:47,
+        borderRadius:5,
+        backgroundColor:'#788eec',
+        width:80,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    buttonText:{
+        color:'white',
+        fontSize:20,
+
+    },
+    noteIcon:{
+        marginTop:5,
+        fontSize:20,
+        marginLeft:14
+    }
+})
